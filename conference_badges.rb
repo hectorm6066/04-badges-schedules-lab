9efbeadf905d839bf1295ec4836a547a55d2eb94
Hector Marin <hectorm6066@hstat.org>
2018-10-29 16:53:43 +0000

    Partner

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+      attendees.collect do |attendee|
+          badge_maker(attendee)
+      end
+end
+def assign_rooms(room_assignments)
+    room_assignments.collect do |name, i|
+        i += 1
+        "Hello, #{name}! You'll be assigned to room #{i}!"
+    end
+end
